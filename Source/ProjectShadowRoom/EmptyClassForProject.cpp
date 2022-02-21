ae7e33dca054a34b4d5ec05d0af0791100ca3583
Jackson Woodard <45219062+Kilowattrl@users.noreply.github.com>
2022-02-20 21:19:15 -0500

    Initial Content

---
+++ Source/ProjectShadowRoom/EmptyClassForProject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "ProjectShadowRoom.h"
+#include "EmptyClassForProject.h"
+
+EmptyClassForProject::EmptyClassForProject()
+{
+}
+
+EmptyClassForProject::~EmptyClassForProject()
+{
+}
